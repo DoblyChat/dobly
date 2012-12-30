05cb0efcb3ddc413e41840cdd33c650c45f6722c
fertrig <fertrig@work>
2012-12-30 08:14:03 -0500

    initial loading of conversations using ko and js

--- public/scripts/client/active.js
+++ public/scripts/client/active.js
@@ -1,5 +1,17 @@
+var socket = io.connect(window.location.origin);
+
 $(document).ready(function() {
 	resizeDualConvo();
+
+	var desktopData = JSON.parse($('#desktop').val());
+	var conversationData = JSON.parse($('#data').val());
+	
+	var viewModel = createViewModel(conversationData, desktopData);
+
+	ko.applyBindings(viewModel);
+
+	//viewModel.adjustScrolling();
+	viewModel.desktop.setupSorting();
 });
 
 function resizeDualConvo() {
@@ -13,3 +25,7 @@ function resizeDualConvo() {
 $(window).resize(function() {
 	resizeDualConvo();
 });
+
+window.onbeforeunload = function() { 
+	socket.emit('remove_active_user'); 
+};

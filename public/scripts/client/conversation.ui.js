aa904274eda353ba3982b6aafaec8d1087debdda
Carlos Atencio <atecarlos@gmail.com>
2013-03-04 08:47:30 -0500

    Refactoring conversation.js file. Moving ui elements to separate file

---
+++ public/scripts/client/conversation.ui.js
@@ -0,0 +1,50 @@
+function createConversationResizing(getSelector) {
+  var self = {};
+
+  self.body = function() {
+    var convoHeight = $('#convos').innerHeight();
+    var titleHeightLeft = $(getSelector('.convo-header')).outerHeight();
+    var newMessageHeightLeft = $(getSelector('.convo-new-message')).outerHeight();  
+    $(getSelector('.convo-body')).height(convoHeight - titleHeightLeft - newMessageHeightLeft);
+  };
+
+  return self;
+}
+
+function createConversationScrolling(getSelector) {
+  var self = {};
+
+  self.setup = function() {
+    self.adjust();
+    setupHoverIntent();
+  };
+
+  function setupHoverIntent() {
+    var config = {
+      over: thickBar,
+      timeout: 1000,
+      out: thinBar,
+    };
+    $(getSelector(".nano > .pane")).hoverIntent(config);
+  }
+
+  function thickBar() {
+    $(this).addClass("thickBar");
+    $(this).siblings(".pane").addClass("thickBar");
+  }
+
+  function thinBar() {
+    $(this).removeClass("thickBar");
+    $(this).siblings(".pane").removeClass("thickBar");
+  }
+
+  self.adjust = function() {
+    $(getSelector('.nano')).nanoScroller({ scroll: 'bottom' });
+  };
+
+  self.stop = function() {
+    $(getSelector('.nano')).nanoScroller({ stop: true });
+  };
+
+  return self;
+}

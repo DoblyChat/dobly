69f493093a0911690dfa2099373858795294fa47
Carlos Atencio <atecarlos@gmail.com>
2013-04-03 19:36:13 -0400

    removed ds store

--- public/scripts/client/allConversations.js
+++ public/scripts/client/allConversations.js
@@ -6,41 +6,17 @@ function createAllConversations(desktop, navigation, conversationsObservable) {
     desktop.addAndActivate(conversation);
   };
 
-  self.pairs = [];
+  self.sortedConversations = [];
 
   self.refresh = function () {
-    var sortedConversations = conversationsObservable().sort(function(left, right){
+    self.sortedConversations = conversationsObservable().sort(function(left, right){
       if (left.unreadCounter() == right.unreadCounter()) {
         return left.topic().toLowerCase().localeCompare(right.topic().toLowerCase());
       } else {
         return left.unreadCounter() < right.unreadCounter() ? 1 : -1; 
       }
     });
-
-    self.pairs = [];
-
-    for (var i = 0; i < sortedConversations.length; i = i + 2) {
-      if (i + 1 < sortedConversations.length) {
-        var pair = [sortedConversations[i], sortedConversations[i + 1]];
-      } else {
-        var pair = [sortedConversations[i]];
-      }
-      self.pairs.push(pair);
-    };
   };
-
-  self.unreadCounter = ko.computed(function(){
-    var count = 0;
-    ko.utils.arrayForEach(conversationsObservable(), function(conversation){
-      count += conversation.unreadCounter();
-    });
-
-    return count;
-  });
-
-  self.showUnreadCounter = ko.computed(function(){
-    return self.unreadCounter() > 0;
-  });
 
   return self;
 }

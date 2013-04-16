a2070c37d2d4e77f44e77dda9bf441674ca4d621
Carlos Atencio <atecarlos@gmail.com>
2013-04-16 08:27:51 -0400

    Complete tests for user io

--- sockets/user_io.js
+++ sockets/user_io.js
@@ -1,6 +1,5 @@
 module.exports = (function (){
-  var User = require('../models/user'),
-      self = this;
+  var self = this;
 
   self.userConnected = function(socket){
     socket.join(socket.handshake.user.groupId);
@@ -14,20 +13,20 @@ module.exports = (function (){
 
   self.requestOnlineUsers = function(currentSocket, sockets){
     var connectedUsers = [];
-    var socketsArray = sockets.clients(currentSocket.groupId);
+    var socketsArray = sockets.clients(currentSocket.handshake.user.groupId);
     for(var i = 0; i < socketsArray.length; i++){
         connectedUsers.push(socketsArray[i].handshake.user._id);
     }
 
     currentSocket.emit('receive_online_users', connectedUsers);
-  }
+  };
 
   self.checkForActiveSession = function(socket){
     if(socket.handshake.session.cookie._expires < new Date()){
-      console.error('Session expired for %s where cookie expired at %s at %s', socket.handshake.user.username, socket.handshake.session.cookie._expires, new Date())
+      console.info('Session expired for %s where cookie expired at %s at %s', socket.handshake.user.username, socket.handshake.session.cookie._expires, new Date());
       socket.emit('timeout');
     }
-  }
+  };
 
   return self;
 })();

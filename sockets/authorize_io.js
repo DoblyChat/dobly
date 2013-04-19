82ea33e976d0301c2523af2cd1594c24f4249296
Carlos Atencio <atecarlos@gmail.com>
2013-04-18 22:53:31 -0400

    Tests for authorization module

--- sockets/authorize_io.js
+++ sockets/authorize_io.js
@@ -9,16 +9,17 @@ module.exports = function authorize(data, accept, sessionStore){
         sessionStore.load(sessionID, function (err, session) {
             if (err || !session) {
                 console.warn('Session not found', err);
-                accept("Can't find session", false);
+                accept('Session not found', false);
             } else {
                 User.findById(session.passport.user, function(err, user){
                   if(err || !user){
-                    console.error('Error retrieving user', err);
+                    console.error('User not found', err);
+                    accept('User not found', false);
+                  }else{
+                    data.session = session;
+                    data.user = user._doc;
+                    accept(null, true);  
                   }
-
-                  data.session = session;
-                  data.user = user._doc;
-                  accept(null, true);
                 });
             }
         });

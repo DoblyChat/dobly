dc88e6b1d749d929b4d52a9c77612bfb732d4dee
fertrig <fertrig@work>
2013-04-28 23:37:54 -0400

    tests for conversation - creation

---
+++ specs/client/spec/conversation.spec.js
@@ -0,0 +1,171 @@
+describe("conversation", function() {
+
+	var conversation;
+	var testData;
+
+	beforeEach(function() {
+		testData = testDataConversation();
+	});
+
+  	describe("creation", function() {
+
+  		it("load properties", function() {
+  			conversation = createConversation(testData);
+
+  	  		expect(conversation.id).toEqual('8');
+  	  		expect(conversation.topic()).toEqual("some topic");
+  	  		expect(conversation.createdBy()).toEqual("fernando");
+  	  		expect(conversation.unreadCounter()).toBe(1);
+  	  		expect(conversation.newMessage()).toEqual("");
+  	  		expect(conversation.isLeft()).toBe(false);
+  	  		expect(conversation.isRight()).toBe(false);
+  	  		expect(conversation.messages().length).toBe(2);
+  	  		expect(conversation.active()).toBe(false);
+  	  		expect(conversation.hasFocus()).toBe(false);
+  	  		expect(conversation.ui).toBeDefined();
+  	  		expect(conversation.timestamp).toBe(common.formatTimestamp(testData.timestamp));
+  	  	});
+
+  	  	it("undefined id", function() {
+  	  	  	testData._id = undefined;
+  	  	  	conversation = createConversation(testData);
+
+  	  	  	expect(conversation.id).toBe(0);
+  	  	});
+
+  	  	it("undefined unread", function() {
+  	  	  	testData.unread = undefined;
+  	  	  	conversation = createConversation(testData);
+
+  	  	  	expect(conversation.unreadCounter()).toBe(0);
+  	  	});
+
+		it("pushes messages", function() {
+		  	conversation = createConversation(testData);
+
+		  	expect(conversation.messages().length).toBe(2);
+		  	expect(conversation.messages()[0].lines[0]).toEqual("alpha");
+		  	expect(conversation.messages()[1].lines[0]).toEqual("beta");
+		});
+  	});
+
+  	describe("last messages", function() {
+  	  	it("3 messages", function() {
+  	  	  	testData.messages = [ testDataMessageAlpha(), testDataMessageBeta(), testDataMessageCharlie() ];
+  	  	  	conversation = createConversation(testData);
+
+  	  	  	expect(conversation.lastMessages().length).toBe(2);
+  	  	  	expect(conversation.lastMessages()[0].lines[0]).toBe("beta");
+  	  	  	expect(conversation.lastMessages()[1].lines[0]).toBe("charlie");
+  	  	});
+
+  	  	it("2 messages", function() {
+  	  	  	testData.messages = [ testDataMessageAlpha(), testDataMessageBeta() ];
+  	  	  	conversation = createConversation(testData);
+
+  	  	  	expect(conversation.lastMessages().length).toBe(2);
+  	  	  	expect(conversation.lastMessages()[0].lines[0]).toBe("alpha");
+  	  	  	expect(conversation.lastMessages()[1].lines[0]).toBe("beta");
+  	  	});
+
+  	  	it("1 messages", function() {
+  	  	  	testData.messages = [ testDataMessageAlpha() ];
+  	  	  	conversation = createConversation(testData);
+
+  	  	  	expect(conversation.lastMessages().length).toBe(1);
+  	  	  	expect(conversation.lastMessages()[0].lines[0]).toBe("alpha");
+  	  	});
+  	});
+
+  	describe("unread counter", function() {
+  	  	it("0 messages", function() {
+  	  	  	testData.unread = 0;
+  	  	  	conversation = createConversation(testData);
+  	  	  	expect(conversation.showUnreadCounter()).toBe(false);
+  	  	});
+
+  	  	it("1 message", function() {
+  	  	  	testData.unread = 1;
+  	  	  	conversation = createConversation(testData);
+  	  	  	expect(conversation.showUnreadCounter()).toBe(true);
+  	  	});
+
+  	  	it("2 messages", function() {
+  	  	  	testData.unread = 2;
+  	  	  	conversation = createConversation(testData);
+  	  	  	expect(conversation.showUnreadCounter()).toBe(true);
+  	  	});
+  	});
+
+  	describe("has focus", function() {
+  	  	it("marks as read", function() {
+  	  		testData.unread = 1;
+  	  	  	conversation = createConversation(testData);
+  	  	  	expect(conversation.unreadCounter()).toBe(1);
+  	  	  	app.socket = createMockSocket();
+
+  	  	  	conversation.hasFocus(true);
+
+  	  	  	expect(conversation.unreadCounter()).toBe(0);
+  	  	  	expect(app.socket.emit).toHaveBeenCalledWith('mark_as_read', '8');
+  	  	});
+
+  	  	it("does not mark as read", function() {
+  	  		testData.unread = 0;
+  	  	  	conversation = createConversation(testData);
+  	  	  	app.socket = createMockSocket();
+
+  	  	  	conversation.hasFocus(true);
+  	  	  	
+  	  	  	expect(app.socket.emit).not.toHaveBeenCalled();
+  	  	});
+  	});
+
+  	function testDataConversation() {
+		return {
+			_id: "8",
+			createdBy: "fernando",
+			groupId: "5",
+			messages: [ testDataMessageAlpha(), testDataMessageBeta() ],
+			timestamp: "2013-02-15T14:36:43.296Z",
+			topic: "some topic",
+			unread: 1,
+		};
+	}
+
+	function testDataMessageAlpha() {
+		return {
+  			content: "alpha", 
+  			conversationId: "8", 
+  			timestamp: Date.parse('2013.04.09 22:13:34'), 
+  			createdBy: "carlos"
+  		};
+	}
+
+	function testDataMessageBeta() {
+		return {
+			content: "beta", 
+			conversationId: "8", 
+			timestamp: Date.parse('2013.04.09 22:14:14'), 
+			createdBy: "fernando"
+		};
+	}
+
+	function testDataMessageCharlie() {
+		return {
+  			content: "charlie", 
+  			conversationId: "8", 
+  			timestamp: Date.parse('2013.04.09 22:15:23'), 
+  			createdBy: "carlos"
+  		};
+	}
+
+	function testDataMessageDelta() {
+		return {
+			content: "delta", 
+			conversationId: "8", 
+			timestamp: Date.parse('2013.04.09 23:18:44'), 
+			createdBy: "fernando"
+		};
+	}
+});

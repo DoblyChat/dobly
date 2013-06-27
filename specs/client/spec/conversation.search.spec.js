6b1c53b78843f0808b754e662e12e9bdb372052f
fertrig <fertrig@work>
2013-06-27 09:53:54 -0400

    skeleton of tests for convo search

---
+++ specs/client/spec/conversation.search.spec.js
@@ -0,0 +1,44 @@
+describe("conversation search", function() {
+
+	describe("next", function() {
+	  	it("does not search when empty", function() {
+	  	  
+	  	});
+
+	  	it("resets", function() {
+	  	  
+	  	});
+
+	  	it("does not reset", function() {
+	  	  
+	  	});
+
+	  	it("finds next", function() {
+	  	  
+	  	});
+
+	  	it("does not find next", function() {
+	  	  
+	  	});
+
+	  	it("scrolls to match when next found", function() {
+	  	  
+	  	});
+
+	  	it("highlights match when next found", function() {
+	  	  
+	  	});
+
+	  	it("pages if next not found", function() {
+	  	  
+	  	});
+
+	  	it("does not page", function() {
+	  	  
+	  	});
+
+	  	it("pages", function() {
+	  	  
+	  	});
+	});
+});

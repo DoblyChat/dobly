b486da91c19ac6fbab33040f50be8a1cb16dc867
fertrig <fertrig@work>
2013-07-24 00:17:33 -0400

    re-fixed jslint issues after merge

--- migrations/004-add-group-raw-name.js
+++ migrations/004-add-group-raw-name.js
@@ -1,29 +1,29 @@
 exports.up = function(next){
-	var Group = require('../models/group'),
-		async = require('async'),
-		helper = require('./helper');
+    var Group = require('../models/group'),
+        async = require('async'),
+        helper = require('./helper');
 
-	helper.connect(function(){
-		helper.disconnect(next);
-	});
+    helper.connect(function(){
+        helper.disconnect(next);
+    });
 
-	function execute() {
-		Group.find({}, function(err, groups){
-			helper.logError(err);
+    function execute() {
+        Group.find({}, function(err, groups){
+            helper.logError(err);
 
-			async.each(groups, update, function(err){
-				helper.logError(err);
-				helper.disconnect(next);
-			});
+            async.each(groups, update, function(err){
+                helper.logError(err);
+                helper.disconnect(next);
+            });
 
-			function update(group, callback){
-				group.rawName = group.name.charAt(0).toUpperCase() + group.name.substring(1).toLowerCase();
-				group.save(callback);
-			}
-		});
-	}
+            function update(group, callback){
+                group.rawName = group.name.charAt(0).toUpperCase() + group.name.substring(1).toLowerCase();
+                group.save(callback);
+            }
+        });
+    }
 };
 
 exports.down = function(next){
-  	next();
+    next();
 };

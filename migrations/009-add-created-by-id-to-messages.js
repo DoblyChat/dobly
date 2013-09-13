00ceef19446b154a7e85aca330cdc6d2e3ef7fb0
fertrig <fertrig@work>
2013-09-12 22:21:18 -0400

    migration: adding created-by-id to messages

---
+++ migrations/009-add-created-by-id-to-messages.js
@@ -0,0 +1,43 @@
+
+exports.up = function(next){
+
+	var Message = require('../lib/models/message'),
+		CollaborationObject = require('../lib/models/collaboration_object'),
+		User = require('../lib/models/user'),
+		async = require('async'),
+		helper = require('./helper');
+
+	helper.connect(function() {
+		execute();
+	});
+
+	function execute() {
+		Message.find({}, function(err, messages) {
+			helper.logError(err);
+
+			async.each(messages, update, function(err) {
+				helper.logError(err);
+				helper.disconnect(next);
+			});
+
+			function update(message, callback) {
+				if (message._doc.createdBy) {
+					CollaborationObject.findById(message.collaborationObjectId, function(err, collaborationObject) {
+						helper.logError(err);
+						User.findOne({ groupId: collaborationObject.groupId, firstName: message._doc.createdBy }, function(err, user) {
+							helper.logError(err);
+							message.createdById = user._id;
+							message.save(callback);
+						})
+					});
+				} else {
+					callback();
+				}
+			}
+		});
+	}
+};
+
+exports.down = function(next){
+	next();
+};

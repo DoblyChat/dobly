ab94ec7063eecceee5905ce9e82074463bac7306
Carlos Atencio <atecarlos@gmail.com>
2013-09-15 15:24:05 -0400

    Task IO object

--- specs/server/sockets.task.spec.js
+++ specs/server/sockets.task.spec.js
@@ -1,127 +1,52 @@
 describe('Sockets', function(){
 	'use strict';
 
-	describe('Tasks', function(){
-		var socketMock, logMock, taskListMock, taskMock, taskIo;
+    describe('Tasks', function(){
+		var taskIo, taskMock,
+			collaborationObjectIo;
 
 		beforeEach(function(){
-			socketMock = {
-				emit: jasmine.createSpy('emit'),
-				handshake: {
-					user: {
-						_id: 'user-id',
-						groupId: 'group-id'
-					}	
-				}
-			};
-
-			logMock = {
-				error: jasmine.createSpy(),
-			};
-
 			mockery.enable({ useCleanCache: true });
 			mockery.registerAllowable('../../lib/sockets/task_io');
 
-			taskListMock = buildMock('../models/task_list', 'create', 'findAllowedTasks', 'findById');
-			logMock = buildMock('../common/log', 'error');
+			collaborationObjectIo = buildMock('./collaboration_object_io', 'sendItem');
+			taskMock = buildMock('../models/task', 'create');
+
 			taskIo = require('../../lib/sockets/task_io');
 		});
-		
-		it('reads task lists', function(){
-			taskIo.readTaskLists(socketMock);
 
-			expect(taskListMock.findAllowedTasks).toHaveBeenCalled();
+		it('saves task', function(){
+			var socket = {
+					handshake: {
+						user: {
+							firstName: 'usr'
+						}
+					}
+				},
+				offlineNotifications = { offline: 'notifications' },
+				data = { da: 'ta' },
+				confirm = jasmine.createSpy();
+
+			taskIo.addTask(socket, offlineNotifications, data, confirm);
+			expect(collaborationObjectIo.sendItem).toHaveBeenCalled();
+			var addArgs = collaborationObjectIo.sendItem.mostRecentCall.args;
+			expect(addArgs[0]).toBe(socket);
+			expect(addArgs[1]).toBe(offlineNotifications);
+			expect(addArgs[2]).toBe(data);
+			expect(addArgs[4]).toBe(confirm);
+
+			var callback = jasmine.createSpy();
+			addArgs[3](callback);
+
+			expect(taskMock.create).toHaveBeenCalled();
 			
-			var args = taskListMock.findAllowedTasks.mostRecentCall.args;
-			expect(args[0]).toBe(socketMock.handshake.user.groupId);
-			expect(args[1]).toBe(socketMock.handshake.user._id);
+			var taskData = taskMock.create.mostRecentCall.args[0];
+			expect(taskData.description).toBe(data.description);
+			expect(taskData.createdById).toBe(socket.handshake.user._id);
+			expect(taskData.timestamp).toBe(data.timestamp);
+			expect(taskData.collaborationObjectId).toBe(data.collaborationObjectId);
 
-			// Error handling
-			args[2]('find error');
-			expect(logMock.error).toHaveBeenCalledWith('Error reading task lists', 'find error');
-			expect(socketMock.emit).not.toHaveBeenCalled();
-
-			// respond
-			var tasks = [ 1, 2 ];
-			args[2](null, tasks);
-			expect(socketMock.emit).toHaveBeenCalledWith('task_lists', tasks);
-		});
-
-		it('creates a list', function(){
-			var data = {
-				forEntireGroup: true,
-				selectedMembers: [ 'selected', 'members' ],
-				name: 'Hello world!'
-			};
-
-			taskIo.createTaskList(socketMock, data);
-			expect(taskListMock.create).toHaveBeenCalled();
-
-			var resultData = taskListMock.create.mostRecentCall.args[0];
-			expect(resultData.createdById).toBe(socketMock.handshake.user._id);
-			expect(resultData.groupId).toBe(socketMock.handshake.user.groupId);
-			expect(resultData.name).toBe(data.name);
-			expect(resultData.members.entireGroup).toBe(true);
-			expect(resultData.members.users).toBe(data.selectedMembers);
-
-			var callback = taskListMock.create.getCallback();
-			
-			// error handling
-			callback('create error');
-			expect(logMock.error).toHaveBeenCalledWith('Error creating task list', 'create error');
-			expect(socketMock.emit).not.toHaveBeenCalled();
-
-			// respond
-			var list = [ 'task 1' ];
-			callback(null, list);
-			expect(socketMock.emit).toHaveBeenCalledWith('task_list_created', list);
-		});
-
-		it('adds a task', function(){
-			var data = {
-				taskListId: '123',
-				description: 'my new task'
-			};
-
-			taskIo.addTask(socketMock, data);
-			expect(taskListMock.findById).toHaveBeenCalled();
-
-			expect(taskListMock.findById.mostRecentCall.args[0]).toBe(data.taskListId);
-
-			var callback = taskListMock.findById.getCallback();
-
-			// error handling
-			callback('find error');
-			expect(logMock.error).toHaveBeenCalledWith('Error finding a task list', 'find error');
-
-			// proceed to save
-			var list = {
-				tasks: [],
-				save: jasmine.createSpy()
-			};
-
-			var expectedTaskList = {
-				complete: false,
-				createdById: socketMock.handshake.user._id,
-				taskListId: data.taskListId,
-				description: data.description
-			};
-			
-			callback(null, list);
-			expect(list.tasks).toContain(expectedTaskList);
-
-			expect(list.save).toHaveBeenCalled();
-
-			callback = list.save.getCallback();
-
-			// error handling
-			callback('save error');
-			expect(logMock.error).toHaveBeenCalledWith('Error creating a task', 'save error');
-			expect(socketMock.emit).not.toHaveBeenCalled();
-
-			// respond
-			callback(null, list);
-			expect(socketMock.emit).toHaveBeenCalledWith('task_created', expectedTaskList);
-		});
+			expect(taskMock.create.getCallback()).toBe(callback);
+		});				
 	});
 });

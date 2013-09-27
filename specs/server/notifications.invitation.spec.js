1c31939c201d7c55b9daf6b66609760b54fa3954
fertrig <fertrig@work>
2013-09-26 20:58:24 -0400

    tests for route.invite and notifications.invitations

---
+++ specs/server/notifications.invitation.spec.js
@@ -0,0 +1,124 @@
+describe("Notifications", function() {
+	'use strict';
+
+	describe("Invitation", function() {
+		var mandrillMock, userMock, groupMock, invitationModelMock;
+		var invitation;
+		var modulePath = '../../lib/notifications/invitation';
+
+		beforeEach(function() {
+			mockery.enable({
+				useCleanCache: true,
+			    warnOnReplace: false,
+			    warnOnUnregistered: false
+			});
+
+			mandrillMock = buildMock('./mandrill_wrapper', 'send');
+			userMock = buildMock('../models/user', 'findById');
+			groupMock = buildMock('../models/group', 'findById');
+			invitationModelMock = buildMock('../models/invitation', 'create');
+		});
+
+		it("sends an email", function(done) {
+			var sendCallback = jasmine.createSpy();
+
+			invitationModelMock.create = function(invitationModel, invitationModelCallback) {
+				expect(invitationModel.email).toEqual('a@b.com');
+				expect(invitationModel.groupId).toEqual('xyz');
+				expect(invitationModel.invitedByUserId).toEqual('123');
+
+				process.env.INVITATION_URL = 'http://www.dobly.com/invitation/';
+				process.env.HOME_PAGE_URL = 'http://www.dobly.com';
+
+				invitationModelCallback(null, { _id: 'ABC456FEG789' });
+
+				expect(mandrillMock.send).toHaveBeenCalled();
+				var args = mandrillMock.send.mostRecentCall.args;
+				expect(args[0]).toEqual('Jacob Smith');
+				expect(args[1]).toEqual('invitation@dobly.com');
+				expect(args[2]).toEqual([ { email : 'a@b.com', name : '' } ]);
+				expect(args[3]).toEqual('no-reply@dobly.com');
+				expect(args[4]).toEqual('You have been invited to Dobly!');
+				expect(args[6]).toEqual(['invitation']);
+				var message = 
+					newLine('Hi there!') + 
+					newLine() + 
+					newLine('Jacob Smith invited you to join his group in Dobly. Use the link below to join:') +
+					newLine() +
+					newLine('http://www.dobly.com/invitation/ABC456FEG789') +
+					newLine() +
+					newLine('Dobly is a very simple tool to help you work better with your group. Learn more about Dobly at our website:') +
+					newLine() +
+					newLine('http://www.dobly.com') +
+					newLine() + 
+					newLine('Cheers,') +
+					newLine() +
+					newLine('The Dobly Team');
+				expect(args[5]).toEqual(message);
+
+				expect(sendCallback).toHaveBeenCalledWith(null);				
+
+				done();
+			};
+
+			groupMock.findById = function(groupId, groupCallback) {
+				groupCallback(null, { _id: 'xyz' });
+			};
+
+			userMock.findById = function(userId, userCallback) {
+				userCallback(null, { 
+					_id: '123',
+					firstName: 'Jacob',
+					lastName: 'Smith',
+					groupId: 'xyz',
+					getFullName: function() { return 'Jacob Smith'}
+				});
+			};			
+
+			invitation = require(modulePath);
+
+		  	invitation.send('123', ['a@b.com'], sendCallback);
+		});
+
+		function newLine(line) {
+			if (line) {
+				return line + '\r\n';
+			} else {
+				return '\r\n';
+			}
+		}
+
+		it("sends two emails", function() {
+		  	var asyncMock = buildMock('async','each');
+		  	var sendCallback = jasmine.createSpy();
+
+		  	invitation = require(modulePath);		  	
+
+		  	invitation.send('123', ['a@b.com','c@d.com'], sendCallback);
+
+		  	expect(asyncMock.each).toHaveBeenCalled();
+		  	var emails = asyncMock.each.mostRecentCall.args[0];
+		  	expect(emails[0]).toEqual('a@b.com');
+		  	expect(emails[1]).toEqual('c@d.com');
+
+		  	mockery.deregisterMock('async');
+		});
+
+		it("handles error", function() {
+		  	var sendCallback = jasmine.createSpy();
+
+			userMock.findById = function(userId, userCallback) {
+				userCallback('some error', null);
+				expect(sendCallback).toHaveBeenCalledWith('some error');
+			};			
+
+			invitation = require(modulePath);
+
+		  	invitation.send('123', ['a@b.com'], sendCallback);
+		});
+
+		afterEach(function() {
+			mockery.disable();
+		});
+	});
+});
---
+++ specs/server/routes.invite.spec.js
@@ -0,0 +1,177 @@
+describe("Routes invite", function() {
+	'use strict';
+
+	var route, req, res;
+	var inviteForm = 'forms/invite';
+	var welcomeForm = 'forms/welcome';
+
+	var invitationMock, logMock, userMock;
+
+	beforeEach(function() {
+		mockery.enable({
+			useCleanCache: true,
+		    warnOnReplace: false,
+		    warnOnUnregistered: false
+		});
+
+		req = {
+			user: {
+				_id: '123'
+			},
+			body: {}
+		};
+		res = { 
+			render: jasmine.createSpy(),
+			locals: {},
+			local: function(field, value) {
+				res.locals[field] = value;
+			}
+		};
+
+		invitationMock = buildMock('../notifications/invitation', 'send');
+		logMock = buildMock('../common/log', 'error');
+		userMock = buildMock('../models/user', 'findById');
+
+		route = require('../../lib/routes/invite');
+	});
+
+	afterEach(function() {
+		mockery.disable();
+	});
+
+	describe("get", function() {
+		it("invite", function() {
+		  	route.get(req, res);
+
+		  	expect(res.render).toHaveBeenCalledWith(inviteForm, { title: APP_TITLE });
+		  	expectFieldsReset();
+		});
+
+		it("welcome", function() {
+			route.getWelcome(req, res);
+
+		  	var callback = getUserCallback();
+		  	callback(null, { firstName: 'Mario' });
+
+		  	expect(res.render).toHaveBeenCalledWith(welcomeForm, { title: APP_TITLE });
+		  	expect(res.locals.userFirstName).toEqual('Mario');
+		  	expectFieldsReset();
+		});
+
+		it("welcome with error", function() {
+		  	route.getWelcome(req, res);
+
+		  	var callback = getUserCallback();
+		  	callback('some error', null);
+
+		  	expect(res.render).toHaveBeenCalledWith(inviteForm, { title: APP_TITLE });
+		  	expect(res.locals.userFirstName).toBeUndefined();
+		  	expectFieldsReset();
+		});
+	});
+
+	function expectFieldsReset() {
+		expect(res.locals.emails).toEqual('');
+		expect(res.locals.invalidEmails).toBe(false);
+		expect(res.locals.inviteError).toBe(false);
+		expect(res.locals.invitationsSent).toBe(false);
+	}
+
+	function getUserCallback() {
+		expect(userMock.findById).toHaveBeenCalled();
+		expect(userMock.findById.mostRecentCall.args[0]).toEqual('123');
+		expect(userMock.findById.mostRecentCall.args[1]).toEqual('firstName');
+		expect(userMock.findById.mostRecentCall.args[2].lean).toBe(true);
+
+		return userMock.findById.mostRecentCall.args[3];
+	}
+
+	describe("post", function() {
+	  	it("validate invite", function() {
+	  	  	req.body.emails = 'b2b.com';
+
+	  	  	route.post(req, res);
+	  	  	
+	  	  	expect(res.render).toHaveBeenCalledWith(inviteForm, { title: APP_TITLE });
+	  	  	expect(res.locals.emails).toEqual('b2b.com');
+	  	  	expect(res.locals.invalidEmails).toBe(true);
+	  	  	expect(res.locals.inviteError).toBe(false);
+	  	  	expect(res.locals.invitationsSent).toBe(false);
+	  	});
+
+	  	it("validate welcome", function() {
+	  	  	req.body.emails = 'a@b.com,b2b.com';
+
+	  	  	route.postWelcome(req, res);
+
+	  	  	var callback = getUserCallback();
+	  	  	callback(null, { firstName: 'Mario' });
+
+  	  		expect(res.render).toHaveBeenCalledWith(welcomeForm, { title: APP_TITLE });
+  	  		expect(res.locals.emails).toEqual('a@b.com,b2b.com');
+  	  		expect(res.locals.invalidEmails).toBe(true);
+  	  		expect(res.locals.inviteError).toBe(false);
+  	  		expect(res.locals.invitationsSent).toBe(false);	  	  	
+	  	});
+
+	  	describe("send invitation", function() {
+
+	  	  	it("error", function() {
+	  	  	  	req.body.emails = 'a@b.com';
+
+	  	  	  	route.post(req, res);
+
+	  	  	  	expect(invitationMock.send).toHaveBeenCalled();
+	  	  	  	
+	  	  	  	var arg_userId = invitationMock.send.mostRecentCall.args[0];
+	  	  	  	var arg_emails = invitationMock.send.mostRecentCall.args[1];
+	  	  	  	var arg_callback = invitationMock.send.mostRecentCall.args[2];
+	  	  	  	
+	  	  	  	expect(arg_userId).toEqual('123');
+	  	  	  	expect(arg_emails.length).toBe(1);
+	  	  	  	expect(arg_emails[0]).toEqual('a@b.com');
+
+	  	  	  	var err = 'silly error';
+	  	  	  	arg_callback(err);
+
+	  	  	  	expect(res.render).toHaveBeenCalledWith(inviteForm, { title: APP_TITLE });
+	  	  	  	expect(res.locals.emails).toEqual('a@b.com');
+	  	  	  	expect(res.locals.invalidEmails).toBe(false);
+	  	  	  	expect(res.locals.inviteError).toBe(true);
+	  	  	  	expect(res.locals.invitationsSent).toBe(false);
+	  	  	  	expect(logMock.error).toHaveBeenCalledWith('silly error');
+	  	  	});
+
+	  	  	it("success", function() {
+	  	  	  	req.body.emails = 'a@b.com,c@d.com';
+
+	  	  	  	route.postWelcome(req, res);
+
+	  	  	  	expect(invitationMock.send).toHaveBeenCalled();
+	  	  	  	
+	  	  	  	var arg_userId = invitationMock.send.mostRecentCall.args[0];
+	  	  	  	var arg_emails = invitationMock.send.mostRecentCall.args[1];
+	  	  	  	var invitation_callback = invitationMock.send.mostRecentCall.args[2];
+	  	  	  	
+	  	  	  	expect(arg_userId).toEqual('123');
+	  	  	  	expect(arg_emails.length).toBe(2);
+	  	  	  	expect(arg_emails[0]).toEqual('a@b.com');
+	  	  	  	expect(arg_emails[1]).toEqual('c@d.com');
+
+	  	  	  	invitation_callback(null);
+
+	  	  	  	var user_callback = getUserCallback();
+
+	  	  	  	user_callback(null, { firstName: 'Mario' });
+	  	  	  	
+  	  	  		expect(res.render).toHaveBeenCalledWith(welcomeForm, { title: APP_TITLE });
+  	  	  		expect(res.locals.emails).toEqual('');
+  	  	  		expect(res.locals.invalidEmails).toBe(false);
+  	  	  		expect(res.locals.inviteError).toBe(false);
+  	  	  		expect(res.locals.invitationsSent).toBe(true);
+
+  	  	  		expect(logMock.error).not.toHaveBeenCalled();	  	  	  
+	  	  	});
+	  	});
+	});
+});

43a9598132efc65783c639ef7e660fa7ad0b45c6
Carlos Atencio <atecarlos@gmail.com>
2013-11-19 09:09:54 -0500

    Tweaked migration to include two at the same time for messages

--- migrations/007-Change-conversation-ID-to-collaborationObjectId.js
+++ migrations/007-Change-conversation-ID-to-collaborationObjectId.js
@@ -3,6 +3,8 @@ exports.up = function(next){
     var Message = require('../lib/models/message'),
   		UnreadMarker = require('../lib/models/unread_marker')
         async = require('async'),
+        CollaborationObject = require('../lib/models/collaboration_object'),
+        User = require('../lib/models/user'),
         helper = require('./helper');
 
     helper.connect(function(){
@@ -23,7 +25,24 @@ exports.up = function(next){
 
             function changeConversationId(item, callback) {
                 item.collaborationObjectId = item._doc.conversationId;
-                item.save(callback);
+                updateCreatedById(item, callback);
+            }
+
+            function updateCreatedById(item, callback) {
+                if (item._doc.createdBy) {
+                    CollaborationObject.findById(item.collaborationObjectId, function(err, collaborationObject) {
+                        helper.logError(err);
+                        if(collaborationObject){
+                            User.findOne({ groupId: collaborationObject.groupId, firstName: item._doc.createdBy }, function(err, user) {
+                                helper.logError(err);
+                                item.createdById = user._id;
+                                item.save(callback);
+                            });
+                        }
+                    });
+                } else {
+                    callback();
+                }
             }
     	});
     }

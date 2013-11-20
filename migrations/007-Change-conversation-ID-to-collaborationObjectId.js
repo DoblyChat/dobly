016102d9265bc976c8baf397b7486f88aade484f
fertrig <fertrig@gmail.com>
2013-11-19 20:24:26 -0500

    updated message migration based on new required field createdById

--- migrations/007-Change-conversation-ID-to-collaborationObjectId.js
+++ migrations/007-Change-conversation-ID-to-collaborationObjectId.js
@@ -3,25 +3,35 @@ exports.up = function(next){
     var Message = require('../lib/models/message'),
   		UnreadMarker = require('../lib/models/unread_marker')
         async = require('async'),
-        helper = require('./helper');
+        helper = require('./helper'),
+        mongo = require('mongoose');
+
+    var dummyCreatedById = new mongo.Types.ObjectId();
 
     helper.connect(function(){
+        console.log('dummy created by id for messages: ' + dummyCreatedById);
         execute();
     });
 
     function execute() {
     	Message.find({}, function(err, messages) {
-    		async.each(messages, changeConversationId, function(err) {
+    		async.each(messages, changeMessageConversationId, function(err) {
     			helper.logError(err);
     			UnreadMarker.find({}, function(err, unreadMarkers) {
-    				async.each(unreadMarkers, changeConversationId, function(err) {
+    				async.each(unreadMarkers, changeUnreadMarkerConversationId, function(err) {
     					helper.logError(err);
     					helper.disconnect(next);
     				});
     			});
     		});
 
-            function changeConversationId(item, callback) {
+            function changeMessageConversationId(item, callback) {
+                item.collaborationObjectId = item._doc.conversationId;
+                item.createdById = dummyCreatedById;
+                item.save(callback);
+            }
+
+            function changeUnreadMarkerConversationId(item, callback) {
                 item.collaborationObjectId = item._doc.conversationId;
                 item.save(callback);
             }
